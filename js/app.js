8387cb0460e5b984ffd668268de0d78cde119ce3
Ahmed Babadr <ahmad@ahmadssb.com>
2015-10-05 15:34:23 +0300

    Feat: Add Drop Animation on marker

--- js/app.js
+++ js/app.js
@@ -11,43 +11,47 @@
 //google.maps.event.addDomListener(window, 'load', initialize);
 
 function initMap() {
-  var myLatLng = {lat: 21.3286303, lng: 39.9535998};
+	'use strict';
+	var myLatLng = {
+		lat: 21.3286303,
+		lng: 39.9535998
+	};
 
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    scrollwheel: false,
-    zoom: 16
-  });
-	
-var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h2 id="firstHeading" class="firstHeading">KACST GIS Technology Innovation Center </h2>'+
-      '<div id="bodyContent">'+
-      '<p>Al Awali, Mecca 24381 </p>'+
-      '<p>Website: <a href="http://www.gistic.org" target="_blank">'+
-      'http://www.gistic.org</a> </p>'+
-      '</div>'+
-      '</div>';
-	
- var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
+	// Create a map object and specify the DOM element for display.
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: myLatLng,
+		scrollwheel: false,
+		zoom: 16
+	});
 
-  // Create a marker and set its position.
-  var marker = new google.maps.Marker({
-    map: map,
-    position: myLatLng,
-    title: 'GISTIC!',
-	 
-  });
-	
-	
- marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
- 
+	var contentString = '<div id="content">' +
+		'<div id="siteNotice">' +
+		'</div>' +
+		'<h2 id="firstHeading" class="firstHeading">KACST GIS Technology Innovation Center </h2>' +
+		'<div id="bodyContent">' +
+		'<p>Al Awali, Mecca 24381 </p>' +
+		'<p>Website: <a href="http://www.gistic.org" target="_blank">' +
+		'http://www.gistic.org</a> </p>' +
+		'</div>' +
+		'</div>';
+
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString
+	});
+
+	// Create a marker and set its position.
+	var marker = new google.maps.Marker({
+		map: map,
+		position: myLatLng,
+		title: 'GISTIC!',
+		animation: google.maps.Animation.DROP 
+	});
+
+
+	marker.addListener('click', function () {
+		infowindow.open(map, marker);
+	});
+
 
 }
-google.maps.event.addDomListener(window, 'load', initMap);
+//google.maps.event.addDomListener(window, 'load', initMap);
