17599747651c41a0b4839983b9082879d017bc4f
Ahmed Babadr <ahmad@ahmadssb.com>
2015-10-05 14:10:10 +0300

    Add Marker

--- js/app.js
+++ js/app.js
@@ -1,11 +1,31 @@
-function initialize() {
-	var mapCanvas = document.getElementById('map');
-	var mapOptions = {
-		
-		center: new google.maps.LatLng(21.3286303, 39.9535998),
-		zoom: 16,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
-	var map = new google.maps.Map(mapCanvas, mapOptions)
+//function initialize() {
+//	var mapCanvas = document.getElementById('map');
+//	var mapOptions = {
+//		
+//		center: new google.maps.LatLng(21.3286303, 39.9535998),
+//		zoom: 16,
+//		mapTypeId: google.maps.MapTypeId.ROADMAP
+//	}
+//	var map = new google.maps.Map(mapCanvas, mapOptions)
+//}
+//google.maps.event.addDomListener(window, 'load', initialize);
+
+function initMap() {
+  var myLatLng = {lat: 21.3286303, lng: 39.9535998};
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    scrollwheel: false,
+    zoom: 16
+  });
+
+  // Create a marker and set its position.
+  var marker = new google.maps.Marker({
+    map: map,
+    position: myLatLng,
+    title: 'GISTIC!',
+	 
+  });
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initMap);
