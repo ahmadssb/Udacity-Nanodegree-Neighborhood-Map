d29de87e98628dccbb749c331e27a363765de670
Ahmed Babadr <ahmad@ahmadssb.com>
2015-10-06 14:26:42 +0300

    Display places list with details

--- js/app.js
+++ js/app.js
@@ -1,49 +1,81 @@
-function initMap() {
-	'use strict';
-	// center map
-	var centerLatLng = {
+// globals
+var marker, i, infowindow;
+
+// center map
+var centerLatLng = {
+	lat: 21.3286303,
+	lng: 39.9535998
+};
+
+// markers data
+var placesData = [
+	{
+		name: 'GISTIC0',
+		desc: 'description about GISTIC0',
+		address: 'address GISTIC0',
+		link: 'http://www.gistic.org',
 		lat: 21.3286303,
 		lng: 39.9535998
-	};
+	},
+	{
+		name: 'GISTIC1',
+		desc: 'description about GISTIC1',
+		address: 'address GISTIC1',
+		link: 'http://www.gistic.org',
+		lat: 21.330729,
+		lng: 39.954801
+	},
+	{
+		name: 'GISTIC2',
+		desc: 'description about GISTIC2',
+		address: 'address GISTIC2',
+		link: 'http://www.gistic.org',
+		lat: 21.328651,
+		lng: 39.953869
+	},
+	{
+		name: 'GISTIC3',
+		desc: 'description about GISTIC3',
+		address: 'address GISTIC3',
+		link: 'http://www.gistic.org',
+		lat: 21.3291708,
+		lng: 39.9482042
+	}];
 
-	// markers data
-	var markersData = [
-		{
-			name: 'GISTIC0',
-			desc: 'description about GISTIC0',
-			address: 'address GISTIC0',
-			link: 'http://www.gistic.org',
-			lat: 21.3286303,
-			lng: 39.9535998
-		},
-		{
-			name: 'GISTIC1',
-			desc: 'description about GISTIC1',
-			address: 'address GISTIC1',
-			link: 'http://www.gistic.org',
-			lat: 21.330729,
-			lng: 39.954801
-		},
-		{
-			name: 'GISTIC2',
-			desc: 'description about GISTIC2',
-			address: 'address GISTIC2',
-			link: 'http://www.gistic.org',
-			lat: 21.328651,
-			lng: 39.953869
-		},
-		{
-			name: 'GISTIC3',
-			desc: 'description about GISTIC3',
-			address: 'address GISTIC3',
-			link: 'http://www.gistic.org',
-			lat: 21.3291708,
-			lng: 39.9482042
-		}];
+var Places = function (data) {
+	this.name = ko.observable(data.name);
+	this.desc = ko.observable(data.desc);
+	this.address = ko.observable(data.address);
+	this.link = ko.observable(data.link);
+	this.lat = ko.observable(data.lat);
+	this.lng = ko.observable(data.lng);
+};
 
-	// globals
-	var infowindow = new google.maps.InfoWindow();
-	var marker, i;
+var ViewModel = {
+	searchBar: ko.observable(''),
+	currentPlace: ko.observable(0),
+
+	init: function () {
+		var self = this;
+		this.placeList = ko.observableArray(placesData);
+	},
+
+	search: function () {
+
+	},
+	setCurrentPlace: function () {
+
+	}
+};
+
+///ViewModel.query.subscribe(ViewModel.search);
+
+ko.applyBindings(ViewModel.init);
+
+
+function initMap() {
+	'use strict';
+	infowindow = new google.maps.InfoWindow();
 
 	// Create a map object and specify the DOM element for display.
 	var map = new google.maps.Map(document.getElementById('map'), {
@@ -54,10 +86,10 @@ function initMap() {
 	});
 
 	// Display markers from markerData
-	for (i = 0; i < markersData.length; i++) {
+	for (i = 0; i < placesData.length; i++) {
 		// setup marker position and animation
 		marker = new google.maps.Marker({
-			position: new google.maps.LatLng(markersData[i].lat, markersData[i].lng),
+			position: new google.maps.LatLng(placesData[i].lat, placesData[i].lng),
 			map: map,
 			animation: google.maps.Animation.DROP
 		});
@@ -69,11 +101,11 @@ function initMap() {
 				var contentString = '<div id="content">' +
 					'<div id="siteNotice">' +
 					'</div>' +
-					'<h4 id="firstHeading" class="firstHeading">' + markersData[currentMarkerI].name + '</h4>' +
+					'<h4 id="firstHeading" class="firstHeading">' + placesData[currentMarkerI].name + '</h4>' +
 					'<div id="bodyContent">' +
-					'<h6>' + markersData[currentMarkerI].desc + '</h6>' +
-					'<p>' + markersData[currentMarkerI].address + '</p>' +
-					'<p>Website: <a href="' + markersData[currentMarkerI].link + '" target="_blank">' + markersData[currentMarkerI].link + '</a> </p>' +
+					'<h6>' + placesData[currentMarkerI].desc + '</h6>' +
+					'<p>' + placesData[currentMarkerI].address + '</p>' +
+					'<p>Website: <a href="' + placesData[currentMarkerI].link + '" target="_blank">' + placesData[currentMarkerI].link + '</a> </p>' +
 					'</div>' +
 					'</div>';
 				infowindow.setContent(contentString);
