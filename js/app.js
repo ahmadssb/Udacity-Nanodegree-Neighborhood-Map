f40b1ba4f8c4214a1c5137f0d3f473ede87eda83
Ahmed Babadr <ahmad@ahmadssb.com>
2015-10-11 19:14:20 +0300

    Feat: Finally Getting selected place Id and Display infoWindow on correct Marker

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // globals
-var marker, i, infowindow, map, contentString;
+var marker, i, map, contentString;
+var infowindow = null;
 
 // center map
 var centerLatLng = {
@@ -17,7 +18,7 @@ var placesData = [
 		link: 'http://www.gistic.org',
 		lat: 21.3286303,
 		lng: 39.9535998,
-		marker: null
+		marker: {}
 	},
 	{
 		name: 'BGISTIC1',
@@ -26,7 +27,7 @@ var placesData = [
 		link: 'http://www.gistic.org',
 		lat: 21.330729,
 		lng: 39.954801,
-		marker: null
+		marker: {}
 	},
 	{
 		name: 'GISTIC2',
@@ -35,7 +36,7 @@ var placesData = [
 		link: 'http://www.gistic.org',
 		lat: 21.328651,
 		lng: 39.953869,
-		marker: null
+		marker: {}
 	},
 	{
 		name: 'GISTIC3',
@@ -44,7 +45,7 @@ var placesData = [
 		link: 'http://www.gistic.org',
 		lat: 21.3291708,
 		lng: 39.9482042,
-		marker: null
+		marker: {}
 	}];
 
 var Places = function (data) {
@@ -52,34 +53,32 @@ var Places = function (data) {
 	this.desc = ko.observable(data.desc);
 	this.address = ko.observable(data.address);
 	this.link = ko.observable(data.link);
-	this.marker = ko.observable(data.marker);
 	this.lat = ko.observable(data.lat);
 	this.lng = ko.observable(data.lng);
+	this.marker = ko.observable(data.marker);
 };
 
 var ViewModel = {
 	searchBar: ko.observable(''),
-	placeList: ko.observableArray([]),
-	currenPlace: ko.observable(),
+	placeList: ko.observableArray(placesData),
 
-	setCurrentPlace: function (clickedPlace) {
-		ViewModel.currenPlace(clickedPlace);
-		//console.log("currenPlace: " + clickedPlace);
+	//currenPlace: ko.observable(this.placeList()[0]),
+
+	setCurrentPlace: function (placeId) {
+		//ViewModel.currenPlace(placeId.toString());
+		console.log("currenPlace: " + placeId.name);
+		console.log("currenPlace: " + placeId.marker);
+		var index = jQuery(ViewModel.placeList()).index(placeId);
+		ViewModel.selectedPlace(index);
+
+		console.log("index: " + index);
+
+
 	},
-
 	init: function () {
 		var self = this;
-		//ViewModel.placeList.clear;
-
-		placesData.forEach(function (place) {
-			ViewModel.placeList.push(new Places(place));
-		});
-
-		ViewModel.currenPlace(ViewModel.placeList()[0]);
-		console.log("init placeList(): ");
+		//this.placeList = ko.observableArray(placesData);
 		console.log(ViewModel.placeList());
-
-		//ViewModel.initMap();
 	},
 
 	query: ko.observable(''),
@@ -88,56 +87,59 @@ var ViewModel = {
 		//ViewModel.placeList.removeAll();
 		if (value === '') {
 			ViewModel.clearMarkers();
-			//console.log('Empty Value return placeData');
-			placesData.forEach(function (place) {
-				ViewModel.placeList.push(new Places(place));
-			});
-			//console.log('ViewModel.placeList()');
-			//console.log(ViewModel.placeList());
+			console.log('Empty Value return placeData');
+			ViewModel.placeList(placesData);
+			console.log('ViewModel.placeList()');
+			console.log(ViewModel.placeList());
 			ViewModel.clearMarkers();
 			ViewModel.addMarkerSet(ViewModel.markers());
-			//console.log('ViewModel.markers()');
-			//console.log(ViewModel.markers());
+			console.log('ViewModel.markers()');
+			console.log(ViewModel.markers());
 		} else {
-			ViewModel.placeList([]);
+			ViewModel.placeList(tempData);
 			ViewModel.placeList.removeAll();
-			//console.log('Before Remove ViewModel.markers()');
-			//console.log(ViewModel.markers());
+			console.log('Before Remove ViewModel.markers()');
+			console.log(ViewModel.markers());
 			ViewModel.clearMarkers();
-			//console.log('After Remove ViewModel.markers()');
-			//console.log(ViewModel.markers());
-			//console.log('Not Empty Value return founded data');
-
-			placesData.forEach(function (place) {
-				for (var x in placesData) {
-					if (placesData[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-						ViewModel.placeList.push(new Places(place));
-						console.log("X: " + x);
-					}
+			console.log('After Remove ViewModel.markers()');
+			console.log(ViewModel.markers());
+			console.log('Not Empty Value return founded data');
+			for (var x in placesData) {
+				if (placesData[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+					ViewModel.placeList.push(placesData[x]);
 				}
-
-			});
-
-			//console.log('Before Add ViewModel.addMarkerSet()');
-			//console.log(ViewModel.markers());
+			}
+			console.log('Before Add ViewModel.addMarkerSet()');
+			console.log(ViewModel.markers());
 			ViewModel.addMarkerSet(ViewModel.placeList());
 			ViewModel.displayMarker();
-			//console.log('After add ViewModel.markers()');
-			//console.log(ViewModel.markers());
+			console.log('After add ViewModel.markers()');
+			console.log(ViewModel.markers());
 		}
 
 	},
 
 
-	contentString: function (currentMarkerI) {
+	contentString: function (currentMarkerI, marker) {
+		console.log(marker);
+		console.log("placeList()[i].marker" + ViewModel.placeList());
+
+		for (i = 0; i < ViewModel.placeList().length; i++) {
+			if (marker === ViewModel.placeList().marker) {
+				console.log(true);
+			} else {
+				console.log(false);
+			}
+		}
+
 		return '<div id="content">' +
 			'<div id="siteNotice">' +
 			'</div>' +
-			'<h4 id="firstHeading" class="firstHeading">' + ViewModel.placeList()[currentMarkerI].name() + '</h4>' +
+			'<h4 id="firstHeading" class="firstHeading">' + ViewModel.placeList()[currentMarkerI].name + '</h4>' +
 			'<div id="bodyContent">' +
-			'<h6>' + ViewModel.placeList()[currentMarkerI].desc() + '</h6>' +
-			'<p>' + ViewModel.placeList()[currentMarkerI].address() + '</p>' +
-			'<p>Website: <a href="' + ViewModel.placeList()[currentMarkerI].link + '" target="_blank">' + ViewModel.placeList()[currentMarkerI].link() + '</a> </p>' +
+			'<h6>' + ViewModel.placeList()[currentMarkerI].desc + '</h6>' +
+			'<p>' + ViewModel.placeList()[currentMarkerI].address + '</p>' +
+			'<p>Website: <a href="' + ViewModel.placeList()[currentMarkerI].link + '" target="_blank">' + ViewModel.placeList()[currentMarkerI].link + '</a> </p>' +
 			'</div>' +
 			'</div>';
 	},
@@ -146,27 +148,34 @@ var ViewModel = {
 
 	// {Doesn't select the correct marker yet} To Open infoWindow for the selected place
 	selectedPlace: function (id) {
+		if (infowindow) {
+			infowindow.close();
+		}
+
 		infowindow = new google.maps.InfoWindow();
+		//infowindow.close();
 		contentString = ViewModel.contentString(id);
 		infowindow.setContent(contentString);
-		infowindow.open(map, marker);
+		console.log("selectedPlaceInfoWind: " + ViewModel.placeList()[id].marker);
+
+		infowindow.open(map, ViewModel.placeList()[id].marker);
 	},
 
 	initMap: function () {
-		//ViewModel.init();
+		//console.log("Before placeList()[i].marker" + ViewModel.placeList()[i].marker);
+
 		map = new google.maps.Map(document.getElementById('map'), {
 			center: centerLatLng,
 			scrollwheel: false,
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
 			zoom: 16
 		});
+		//ViewModel.renderMarkers(map);
 		ViewModel.addMarkerSet(ViewModel.markers());
-		console.log('initMap');
-		console.log(ViewModel.placeList());
+		console.log('markers');
+		console.log(ViewModel.markers());
 
-		//console.log(ViewModel.markers());
-
-		//ViewModel.selectedPlace(0);
+		//ViewModel.selectedPlace(3);
 	},
 
 	addMarker: function (lat, lng, i) {
@@ -177,31 +186,45 @@ var ViewModel = {
 		});
 		ViewModel.displayMarker();
 		ViewModel.markers.push(marker);
-		////console.log("markers: " + ViewModel.markers());
+		ViewModel.placeList()[i].marker = marker;
+		console.log("placeList()[i].marker" + ViewModel.placeList()[i].marker);
+		console.log("marker" + marker);
+		if (marker === ViewModel.placeList()[i].marker) {
+			console.log(true);
+		} else {
+			console.log(false);
+		}
+		if (infowindow) {
+			infowindow.close();
+		}
 
 		// getting the the current (i) when the user click on marker 
 		infowindow = new google.maps.InfoWindow();
 		google.maps.event.addListener(marker, 'click', (function (marker, currentMarkerI) {
 			return function () {
 				// setup content tamplate 
-				contentString = ViewModel.contentString(currentMarkerI);
+				contentString = ViewModel.contentString(currentMarkerI, marker);
 				infowindow.setContent(contentString);
 				infowindow.open(map, marker);
+				if (marker === ViewModel.placeList()[i].marker) {
+					console.log("Compare marker === placeList.marker");
+					console.log(true);
+				} else {
+					console.log(false);
+				}
+				console.log("selectedPlaceInfoWind: " + ViewModel.placeList()[currentMarkerI].marker);
+
 			};
 		})(marker, i));
 	},
 
 	addMarkerSet: function (markerSetArray) {
 		for (i = 0; i < ViewModel.placeList().length; i++) {
-			ViewModel.addMarker(ViewModel.placeList()[i].lat(), ViewModel.placeList()[i].lng(), i);
-			//ViewModel.selectedPlace(i);
-			console.log(ViewModel.placeList()[i].lat());
-
+			ViewModel.addMarker(ViewModel.placeList()[i].lat, ViewModel.placeList()[i].lng, i);
 		}
 	},
 
 	setMarker: function (map) {
-
 		for (var i = 0; i < ViewModel.markers().length; i++) {
 			ViewModel.markers()[i].setMap(map);
 		}
@@ -220,6 +243,7 @@ var ViewModel = {
 		ViewModel.markers = ko.observableArray([]);
 	},
 };
-ViewModel.init();
+
 ViewModel.query.subscribe(ViewModel.search);
+
 ko.applyBindings(ViewModel);
