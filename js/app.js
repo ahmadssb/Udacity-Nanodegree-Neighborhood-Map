8f8c7b76f4fa5a1a109cbee2fd05828ccbd42201
Ahmed Babadr <ahmad@ahmadssb.com>
2015-10-12 17:23:23 +0300

    Display place List and Markers from foursquare api

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ var infowindow = null;
 
 // center map
 var centerLatLng = {
-	lat: 21.3286303,
-	lng: 39.9535998
+	lat: 21.3989712,
+	lng: 39.8004776
 };
 
 // markers data
@@ -53,7 +53,7 @@ var foursquareBaseUri = "https://api.foursquare.com/v2/venues/search?";
 var foursquareClientId = "012U2GL5VVR52WLULMSOXOODKHJW3C53OBTI5ZA4KAYU5BG1";
 var foursquareClientSecret = "43A2ZPGGC1KSP1QMVHT2CDRTD2RCLGCITNIVI5Q1HCNBJK1P";
 var centerLocation = centerLatLng.lat + "," + centerLatLng.lng;
-var limit = 10;
+var limit = 20;
 var extraParams = "&limit=" + limit + "&query=restaurant";
 var foursquareURL = foursquareBaseUri + "client_id=" + foursquareClientId + "&client_secret=" + foursquareClientSecret + "&v=20130815" + "&ll=" + centerLocation + extraParams;
 
@@ -80,17 +80,16 @@ var ViewModel = {
 				ViewModel.foursquareList.push(myData);
 			}
 			console.log(ViewModel.foursquareList());
+			ViewModel.initMap();
 		});
 		console.log(ViewModel.foursquareList());
-
-		ViewModel.initMap()
 	},
 
 	setCurrentPlace: function (placeId) {
 		//ViewModel.currenPlace(placeId.toString());
 		console.log("currenPlace: " + placeId.name);
 		console.log("currenPlace: " + placeId.marker);
-		var index = jQuery(ViewModel.placeList()).index(placeId);
+		var index = jQuery(ViewModel.foursquareList()).index(placeId);
 		console.log("index: " + index);
 		ViewModel.selectedPlace(index);
 	},
@@ -101,30 +100,30 @@ var ViewModel = {
 		if (value === '') {
 			ViewModel.clearMarkers();
 			console.log('Empty Value return placeData');
-			ViewModel.placeList(placesData);
+			ViewModel.foursquareList(foursquareData);
 			console.log('ViewModel.placeList()');
-			console.log(ViewModel.placeList());
+			console.log(ViewModel.foursquareList());
 			ViewModel.clearMarkers();
 			ViewModel.addMarkerSet(ViewModel.markers());
 			console.log('ViewModel.markers()');
 			console.log(ViewModel.markers());
 		} else {
-			ViewModel.placeList(tempData);
-			ViewModel.placeList.removeAll();
+			ViewModel.foursquareList(tempData);
+			ViewModel.foursquareList.removeAll();
 			console.log('Before Remove ViewModel.markers()');
 			console.log(ViewModel.markers());
 			ViewModel.clearMarkers();
 			console.log('After Remove ViewModel.markers()');
 			console.log(ViewModel.markers());
 			console.log('Not Empty Value return founded data');
-			for (var x in placesData) {
-				if (placesData[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-					ViewModel.placeList.push(placesData[x]);
+			for (var x in foursquareData) {
+				if (foursquareData[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+					ViewModel.foursquareList.push(foursquareData[x]);
 				}
 			}
 			console.log('Before Add ViewModel.addMarkerSet()');
 			console.log(ViewModel.markers());
-			ViewModel.addMarkerSet(ViewModel.placeList());
+			ViewModel.addMarkerSet(ViewModel.foursquareList());
 			ViewModel.displayMarker();
 			console.log('After add ViewModel.markers()');
 			console.log(ViewModel.markers());
@@ -135,11 +134,11 @@ var ViewModel = {
 		return '<div id="content">' +
 			'<div id="siteNotice">' +
 			'</div>' +
-			'<h4 id="firstHeading" class="firstHeading">' + ViewModel.placeList()[currentMarkerI].name + '</h4>' +
+			'<h4 id="firstHeading" class="firstHeading">' + ViewModel.foursquareList()[currentMarkerI].name + '</h4>' +
 			'<div id="bodyContent">' +
-			'<h6>' + ViewModel.placeList()[currentMarkerI].desc + '</h6>' +
-			'<p>' + ViewModel.placeList()[currentMarkerI].address + '</p>' +
-			'<p>Website: <a href="' + ViewModel.placeList()[currentMarkerI].link + '" target="_blank">' + ViewModel.placeList()[currentMarkerI].link + '</a> </p>' +
+			// '<h6>' + ViewModel.placeList()[currentMarkerI].desc + '</h6>' +
+			'<p>' + ViewModel.foursquareList()[currentMarkerI].address + '</p>' +
+			// '<p>Website: <a href="' + ViewModel.placeList()[currentMarkerI].link + '" target="_blank">' + ViewModel.placeList()[currentMarkerI].link + '</a> </p>' +
 			'</div>' +
 			'</div>';
 	},
@@ -154,8 +153,8 @@ var ViewModel = {
 		infowindow = new google.maps.InfoWindow();
 		contentString = ViewModel.contentString(id);
 		infowindow.setContent(contentString);
-		console.log("selectedPlaceInfoWind: " + ViewModel.placeList()[id].marker);
-		infowindow.open(map, ViewModel.placeList()[id].marker);
+		console.log("selectedPlaceInfoWind: " + ViewModel.foursquareList()[id].marker);
+		infowindow.open(map, ViewModel.foursquareList()[id].marker);
 	},
 
 	initMap: function () {
@@ -164,11 +163,11 @@ var ViewModel = {
 			center: centerLatLng,
 			scrollwheel: false,
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
-			zoom: 16
+			zoom: 11
 		});
 		ViewModel.addMarkerSet(ViewModel.markers());
 		console.log('markers');
-		console.log(ViewModel.markers());
+		//console.log(ViewModel.markers());
 		//console.log(ViewModel.foursquare());
 		//console.log(ViewModel.foursquareList());
 
@@ -182,14 +181,14 @@ var ViewModel = {
 		});
 		ViewModel.displayMarker();
 		ViewModel.markers.push(marker);
-		ViewModel.placeList()[i].marker = marker;
-		console.log("placeList()[i].marker" + ViewModel.placeList()[i].marker);
-		console.log("marker" + marker);
-		if (marker === ViewModel.placeList()[i].marker) {
-			console.log(true);
-		} else {
-			console.log(false);
-		}
+		ViewModel.foursquareList()[i].marker = marker;
+//		console.log("placeList()[i].marker" + ViewModel.foursquareList()[i].marker);
+//		console.log("marker" + marker);
+//		if (marker === ViewModel.foursquareList()[i].marker) {
+//			console.log(true);
+//		} else {
+//			console.log(false);
+//		}
 		if (infowindow) {
 			infowindow.close();
 		}
@@ -202,21 +201,21 @@ var ViewModel = {
 				contentString = ViewModel.contentString(currentMarkerI, marker);
 				infowindow.setContent(contentString);
 				infowindow.open(map, marker);
-				if (marker === ViewModel.placeList()[i].marker) {
-					console.log("Compare marker === placeList.marker");
-					console.log(true);
-				} else {
-					console.log(false);
-				}
-				console.log("selectedPlaceInfoWind: " + ViewModel.placeList()[currentMarkerI].marker);
+//				if (marker === ViewModel.foursquareList()[i].marker) {
+//					console.log("Compare marker === foursquareList.marker");
+//					console.log(true);
+//				} else {
+//					console.log(false);
+//				}
+				console.log("selectedPlaceInfoWind: " + ViewModel.foursquareList()[currentMarkerI].marker);
 
 			};
 		})(marker, i));
 	},
 
 	addMarkerSet: function (markerSetArray) {
-		for (i = 0; i < ViewModel.placeList().length; i++) {
-			ViewModel.addMarker(ViewModel.placeList()[i].lat, ViewModel.placeList()[i].lng, i);
+		for (i = 0; i < ViewModel.foursquareList().length; i++) {
+			ViewModel.addMarker(ViewModel.foursquareList()[i].lat, ViewModel.foursquareList()[i].lng, i);
 		}
 	},
 
